fd8af4955675303e95a650472544fefa03a840da
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-10-16 13:24:11 -0300

    feat: create tableProvider

---
+++ src/contexts/tableProvider/index.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext, useState } from "react";
+
+export const TableContext = createContext();
+
+export const TableProvider = ({ children }) => {
+  const [table, setTable] = useState({ current_round: 0 });
+
+  return (
+    <TableContext.Provider value={{ table, setTable }}>
+      {children}
+    </TableContext.Provider>
+  );
+};
+
+export const useTable = () => useContext(TableContext);

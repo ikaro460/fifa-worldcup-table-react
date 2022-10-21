2c4d16f81d34ce3e5c17b950ad2caed907da2f93
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-10-21 13:34:07 -0300

    feat!: Sim all matches and groups

--- src/components/Match/index.jsx
+++ src/components/Match/index.jsx
@@ -1,9 +1,8 @@
-//SIMULAR PARTIDAS ->
-
 import produce from "immer";
 import { useContext } from "react";
 import { MatchesContext } from "../../contexts/matchProvider";
 import { TableContext } from "../../contexts/tableProvider";
+import { matchMaking } from "../../utils/matchUtils";
 
 export const Match = () => {
   const { table, setTable } = useContext(TableContext);
@@ -22,34 +21,67 @@ export const Match = () => {
         ? team1
         : team2;
 
-    const looser = winner === team1 ? team2 : team1;
+    const loser = winner === team1 ? team2 : team1;
 
-    const results = {
+    const matchResults = {
       group_id: table.groups[group].group_id,
       team_1: table.groups[group].teams[team1].Name,
       team_2: table.groups[group].teams[team2].Name,
       scoreTeam1: scoreTeam1,
       scoreTeam2: scoreTeam2,
       winner: winner,
+      loser: loser,
     };
 
+    return matchResults;
+  };
+
+  const simulateGroup = (group) => {
+    let newGroupResults = [];
+
+    for (let i = 0; i < matchMaking.length; i++) {
+      const result = simulateMatch(group, matchMaking[i][0], matchMaking[i][1]);
+      newGroupResults.push(result);
+    }
+
+    return newGroupResults;
+    // updateStates(group, newMatchesResults);
+  };
+
+  const SimGroupStage = () => {
+    let newMatchesResults = [];
+
+    for (let i = 0; i < table.groups.length; i++) {
+      const result = simulateGroup(i);
+      newMatchesResults.push(result);
+    }
+    updateStates(newMatchesResults);
+  };
+
+  const updateStates = (newMatchesResults) => {
     const newMatches = produce(matches, (draft) => {
-      draft.group_stage.results.push(results);
+      draft.group_stage.results.push([newMatchesResults]);
     });
+
     setMatches(newMatches);
 
     const newTable = produce(table, (draft) => {
-      draft.groups[group].teams[winner].team_stats.Wins += 1;
-      draft.groups[group].teams[winner].team_stats.Points += 3;
+      newMatchesResults.forEach((group, groupIndex) => {
+        group.forEach((match) => {
+          draft.groups[groupIndex].teams[match.winner].team_stats.Wins += 1;
+          draft.groups[groupIndex].teams[match.winner].team_stats.Points += 3;
+          draft.groups[groupIndex].teams[match.loser].team_stats.Losses += 1;
+        });
+      });
     });
+
     setTable(newTable);
   };
 
   return (
     <div>
-      <button onClick={() => simulateMatch("0", "0", "1")}>
-        Advance Round
-      </button>
+      <button onClick={() => simulateMatch(0, 0, 1)}>Advance Round</button>
+      <button onClick={() => SimGroupStage()}>Sim Groups</button>
     </div>
   );
 };

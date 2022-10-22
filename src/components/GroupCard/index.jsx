24ff1231e0a07386a21eea770ecad8a25c621960
Ícaro Nascimento <ikaro_460@hotmail.com>
2022-10-21 22:30:52 -0300

    feat!: implement MUI DataGrid

--- src/components/GroupCard/index.jsx
+++ src/components/GroupCard/index.jsx
@@ -1,4 +1,4 @@
-import { GroupTeamCard } from "../GroupTeamCard";
+import { DataGrid } from "@mui/x-data-grid";
 
 export const GroupCard = ({ group }) => {
   const auxArr = [...group.teams];
@@ -7,23 +7,53 @@ export const GroupCard = ({ group }) => {
     return b.team_stats.Points - a.team_stats.Points;
   });
 
+  const rows = [];
+
+  sortedArr.map((element, index) => {
+    return rows.push({
+      ...element.team_stats,
+      Name: element.Name,
+      id: index + 1,
+    });
+  });
+
+  console.log(sortedArr, rows);
+
+  const columns = [
+    { field: "id", headerName: "Pos", type: "number" },
+    { field: "Name", headerName: "Team", width: "100" },
+    { field: "Wins", headerName: "W", type: "number" },
+    { field: "Losses", headerName: "L", type: "number" },
+    { field: "Points", headerName: "Points", type: "number" },
+  ];
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>*</th>
-          <th>Team</th>
-          <th>W</th>
-          <th>L</th>
-          <th>D</th>
-          <th>Pts</th>
-        </tr>
-      </thead>
-      <tbody>
-        {sortedArr.map((team, index) => {
-          return <GroupTeamCard team={team} key={index} index={index} />;
-        })}
-      </tbody>
-    </table>
+    <div style={{ width: "50%" }}>
+      <DataGrid
+        rows={rows}
+        columns={columns}
+        hideFooter
+        rowHeight={25}
+        autoHeight
+        disableColumnMenu
+      />
+    </div>
+    // <table>
+    //   <thead>
+    //     <tr>
+    //       <th>*</th>
+    //       <th>Team</th>
+    //       <th>W</th>
+    //       <th>L</th>
+    //       <th>D</th>
+    //       <th>Pts</th>
+    //     </tr>
+    //   </thead>
+    //   <tbody>
+    //     {sortedArr.map((team, index) => {
+    //       return <GroupTeamCard team={team} key={index} index={index} />;
+    //     })}
+    //   </tbody>
+    // </table>
   );
 };
